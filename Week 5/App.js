31cb5a4a45b99d9c7ddbc1edfe64534c1e6ada9f
Sakkeyy <sukarid@hotmail.com>
2024-02-12 23:40:06 +0200

    Week 5 & 6 done with screenshots

---
+++ Week 5/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import Map from './screens/Map'
+import Constants from 'expo-constants'
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Map/>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: Platform.OS === 'ios' ? Constants.statusBarHeight : 0
+  },
+})
---
+++ Week 6/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Position from './components/Position';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Current Weather</Text>
+      <Position/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'cente',
+  },
+
+
+});
